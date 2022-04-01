d3d089ac7b84aa92646e5721e698ecb32cde6f62
Pedro Luiz Camara <camarapedro139@gmail.com>
2022-04-01 11:07:55 -0300

    Add files via upload

---
+++ LDEOrdenada/Cliente.java
@@ -0,0 +1,63 @@
+package LDETeste;
+
+public class Cliente implements Comparable <Cliente>{
+
+    private String cpf;
+    private String nome;
+    private String fone;
+    private String email;
+
+    public Cliente(String c) {
+        this.cpf = c;
+    }
+
+    public Cliente(String c, String n, String f, String e) {
+        this.cpf = c;
+        this.nome = n;
+        this.fone = f;
+        this.email = e;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [cpf=" + cpf + ", email=" + email + ", fone=" + fone + ", nome=" + nome + "]";
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int compareTo(Cliente c) {
+        return this.email.compareTo(c.email);
+    }
+
+}
